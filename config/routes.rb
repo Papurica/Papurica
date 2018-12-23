b2d125015dd619565ff15251ad31c7fa6282a5ce
Hikaru Tanaka <hikatan@papurica.jp>
2018-12-23 21:00:44 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   resources :articles do
     resources :comments
   end
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
   post 'articles' => 'articles#create'
+  post '/signup',  to: 'users#create'
 end
