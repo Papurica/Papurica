5e4df6267f49ae54ce89227b809c4e805b13b299
Takuto Yoshikai <takuto.yoshikai@gmail.com>
2018-12-24 13:42:14 +0900

    thanksのモデルを追加&アソシエーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_040427) do
+ActiveRecord::Schema.define(version: 2018_12_24_043811) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2018_12_24_040427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "comment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
